eaa304816ab6a0c73297663f1dfc65e773f83be1
Diego Rocha <107161911+odiegorocha@users.noreply.github.com>
2025-07-08 03:30:21 -0300

    Create NumEmLista.c

---
+++ Atividades Complementares/NumEmLista.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int lista[50];
+    int numero, i, indice = -1, indice_maior = 0;
+
+    srand(time(NULL));
+
+    for (i = 0; i < 50; i++) {
+        lista[i] = rand() % 100;
+        printf("%d ", lista[i]);
+    }
+    printf("\n");
+
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    for (i = 0; i < 50; i++) {
+        if (lista[i] == numero) {
+            indice = i;
+            break;
+        }
+    }
+
+    for (i = 1; i < 50; i++) {
+        if (lista[i] > lista[indice_maior]) {
+            indice_maior = i;
+        }
+    }
+
+    if (indice != -1) {
+        printf("Índice do número %d: %d\n", numero, indice);
+    } else {
+        printf("Número %d nao encontrado: -1.\n", numero);
+    }
+
+    printf("Índice do maior valor (%d): %d\n", lista[indice_maior], indice_maior);
+
+    return 0;
+}
